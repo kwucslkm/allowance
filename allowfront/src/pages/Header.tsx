a69ed51957e703d99d7b016cd13efd3ddebd2480
kwucslkm <kwucsani@naver.com>
2025-01-08 15:35:14 +0900

    commit 2 20250108-2

--- allowfront/src/pages/Header.tsx
+++ allowfront/src/pages/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styles from '../styles/Header.module.css'; // CSS 파일 import
 
 const Header: React.FC = () => {
   return (
-    <div>
-      <h2>I am header!!!</h2>
-    </div>
+    <header>
+      <div className='header'>
+        <h2>I am header!!!</h2>
+      </div>
+    </header>
   );
 };
 
