c4b7c9a920de01cfa36eb3c8cf01e76f9fc3346d
kwucslkm <kwucsani@naver.com>
2025-01-11 03:37:59 +0900

    지출내역,금액  form, input 작성 20250111-1

--- allowfront/src/pages/Home.tsx
+++ allowfront/src/pages/Home.tsx
@@ -9,10 +9,10 @@ const Home: React.FC = () => {
     fetchAllowances().then(setAllowances);
   }, []);
 
-  const handleAdd = async () => {
+  const handleAdd = async (desc,amount) => {
     const newAllowance = await createAllowance({
-      description: '피자',
-      amount: 1,
+      description: desc,
+      amount: amount,
       date: new Date().toISOString(),
     });
     setAllowances((prev) => [...prev, newAllowance]);
@@ -23,16 +23,34 @@ const Home: React.FC = () => {
   //   public amount!: number;
   //   public date!: Date;
   // }
+  
   interface Allowance  {
     id: number;
     description: string;
     amount: number;
     date: Date;
   }
+  
   return (
-    <article>
-      <div className = "Home">
+   <div className = "Home">
+        <form onSubmit={(e)=>{
+          const desc = e.target.description.value;
+          const amount = e.target.amount.value;
+          handleAdd(desc,amount);
+        }}>
+          <div>오늘 사용한 지출을 입력하세요</div>
+          <input type="text" name = "description" placeholder ="지출내역을 입력하세요"/>
+          <input type="number" name = "amount" placeholder="지출금액을 입력하세요"/>
+          <input type="submit" onClick={(e)=>{
+            e.priventDefault();
+
+          }} value="지출입력"/>
+        </form>
         
+        
+        
+        {/* 지출 추가 : <button onClick={handleAdd}>Add Allowance</button> */}
+        <h3>지출내역</h3>
         <ul>
           {allowances.map((allowance: Allowance) => (
             <li key={allowance.id}>
@@ -40,9 +58,9 @@ const Home: React.FC = () => {
             </li>
           ))}
         </ul>
-        지출 추가 : <button onClick={handleAdd}>Add Allowance</button>
+        
       </div>
-    </article>
+    
   );
 };
 
