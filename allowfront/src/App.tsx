d6a8f0d8abb49acaac70396aac9f719bb6d23279
kwucslkm <kwucsani@naver.com>
2025-01-15 16:31:36 +0900

    세션 시작 2025011504

--- allowfront/src/App.tsx
+++ allowfront/src/App.tsx
@@ -24,7 +24,25 @@
       if(memberLoginCheck.success){
         setLoginYn(true);
         setShowLoginForm(false);
-        alert(memberLoginCheck.message);
+        const handleLogin = (userId: string) => {
+          sessionStorage.setItem('userId', userId);
+          console.log('User ID stored in session:', userId);
+        };
+        // 세션 읽기
+        // const getUserId = () => {
+        //   return sessionStorage.getItem('userId');
+        // };
+        
+        // const userId = getUserId();
+        // console.log('Logged-in User ID:', userId);
+        
+        // alert(memberLoginCheck.message);
+        // 로그아웃 시 아이디 제거:
+        // const handleLogout = () => {
+        //   sessionStorage.removeItem('userId');
+        //   console.log('User ID removed from session');
+        // };
+        
       }else {
         alert('다시 확인 하시고 시도 하세요');
         // setShowLoginForm(true);
