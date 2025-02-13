d8e4f54be41c4f31a53c77ed2f9ac29c40962c5f
kwucslkm <kwucsani@naver.com>
2025-02-13 12:05:18 +0900

    ssl 수정정 2025-02-13-5

--- allowback/src/routes/allowancesRouter.ts
+++ allowback/src/routes/allowancesRouter.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import Allowances from '../models/Allowance'
 import Member from './../models/Member';
 import bcrypt from 'bcryptjs';
-// import bcrypt from 'bcrypt';
 import bodyParser from 'body-parser';
 
 const router = express.Router();
